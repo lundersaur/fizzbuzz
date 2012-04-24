1a8c46a368e80a039764178491cf78ea30d39fde
Ian Lunderskov <ilunderskov@gmail.com>
2012-04-23 21:09:38 -0500

    First commit, python and ruby buzzes

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+for i in 1..20
+   if i % 3 == 0 && i % 5 == 0
+      print "FizzBuzz\n"
+   elsif i % 3 == 0
+      print "Fizz\n"
+   elsif i % 5 == 0
+      print "Buzz\n"
+   else
+      print i,"\n"
+   end
+end
