fdaad76edc12ba8fc5f32ef951f0064ac6e4cd94
Ian Lunderskov <ilunderskov@gmail.com>
2012-12-03 23:32:56 -0600

    Updating to increment to 100.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-for i in range(1,21):
+for i in range(1,101):
    if i % 3 == 0 and i % 5 == 0:
       print("FizzBuzz")
    elif i % 3 == 0:
--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,4 @@
-for i in 1..20
+for i in 1..100
    if i % 3 == 0 && i % 5 == 0
       print "FizzBuzz\n"
    elsif i % 3 == 0
